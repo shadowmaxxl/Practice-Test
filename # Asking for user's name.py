081fdfe8f208d9d4d98990cbc7b83335f81038d9
shadowmaxxl <kharlfurguson@yahoo.com>
2023-04-25 01:59:16 -0400

    just random code to improve my development.
    what you see here is a bunch of code that display inputs, loops, and strings basically lot of functions in play. I am still learning to code in python I am not proficient yet.

---
+++ # Asking for user's name.py
@@ -0,0 +1,29 @@
+# Asking for user's name
+name = input("What is your name? ")
+
+# Asking for user's gender
+gender = input("What is your gender? ")
+
+# Asking for user's location
+location = input("Where do you live? ")
+
+# Printing out the collected information
+print(f"Hello {name}! You identified as {gender} and live in {location}.")
+
+mood = input("are you horny or happy? ")
+
+position = input("what are your hardcore positions you like to do? ")
+
+choices = input("do you like to have sex on the, bed? or floor? ")
+
+surfaces = 'floor'
+if name.lower() == 'floor':
+    print("you {name} are a sex driven crazy motherfucker, get the FUCK OUT!")
+
+elif name.lower() == 'bed':
+    print("you {name} are a freaky freak i hope your mom knows that you were a sex driven bandit")    
+
+else:
+    print(f"you better leave {location} or you're {mood} ass will be on the {choices} you kinky bitch :P")
+    
+    print("what will you do, if someone want to to do a three-some? {surfaces}")
